afbae87432818456da0ff91800e5ee0d19940647
ken-sn00py <b2211720@photon.chitose.ac.jp>
2022-10-31 15:09:26 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+
+        int studentNumber = 2211720;
+        System.out.println("B"+studentNumber+" Kengo Nagata");
+
+    }
+}
