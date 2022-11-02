6ef1db8eefab3945f8798bfc1aca6f1d53e1dcf8
ken-sn00py <b2211720@photon.chitose.ac.jp>
2022-11-02 11:10:06 +0900

    課題2

--- prmn2022a/src/lecture02/kadai22.java
+++ prmn2022a/src/lecture02/kadai22.java
@@ -3,7 +3,7 @@ package lecture02;
 public class kadai22 {
     public static void main(String[] args) {
 
-//        Car0 car = new Car0();
+//        Car car = new Car();
 //        GasStation gas = new GasStation();
 
 //        car.run();
