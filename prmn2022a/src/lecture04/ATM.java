aae4652ba6d2510ffce10aa6bc4eb5f4067a536d
ken-sn00py <b2211720@photon.chitose.ac.jp>
2022-11-22 16:12:35 +0900

    課題4-2

--- prmn2022a/src/lecture04/ATM.java
+++ prmn2022a/src/lecture04/ATM.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class ATM {
 
     ArrayList<Account> accountList = new ArrayList<>();
-    Account account = new Account("藤井智子","12345");
+    //Account account = new Account("藤井智子","12345");
 
     ATM(){
-        accountList.add(account);
+        accountList = new ArrayList<>();
     }
 
     public void registerAccount(String name,String number){
@@ -21,17 +21,21 @@ public class ATM {
     }
 
     public void deposit(String number,long money){
-        account.setBalance(account.getBalance() + money);
-        System.out.println("口座番号："+number+"　に"+money+"円入金しました。");
+        for(Account account : accountList) {
+            account.setBalance(account.getBalance() + money);
+            System.out.println("口座番号：" + number + "　に" + money + "円入金しました。");
+        }
     }
 
     public long withdraw(String number,long money){
-        if(account.getBalance()>money) {
-            account.setBalance(account.getBalance() - money);
-            System.out.println("口座番号："+number+"　から"+money+"円引き出しました。残高："+account.getBalance()+"円です");
-        }else {
-            System.out.println("口座番号："+number+"　から"+money+"円引き出せませんでした。残高："+account.getBalance()+"円です");
+        for(Account account : accountList) {
+            if (account.getBalance() > money) {
+                account.setBalance(account.getBalance() - money);
+                System.out.println("口座番号：" + number + "　から" + money + "円引き出しました。残高：" + account.getBalance() + "円です");
+            } else {
+                System.out.println("口座番号：" + number + "　から" + money + "円引き出せませんでした。残高：" + account.getBalance() + "円です");
+            }
         }
-        return account.getBalance();
+            return money;
     }
 }
---
+++ prmn2022a/src/lecture05/Insect.java
@@ -0,0 +1,4 @@
+/*package lecture05;
+
+public class Insect {
+}*/
