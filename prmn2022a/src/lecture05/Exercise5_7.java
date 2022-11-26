a51732c5df649437a98cae31adcd5513e1ab8f4d
ken-sn00py <b2211720@photon.chitose.ac.jp>
2022-11-26 13:04:17 +0900

    課題5-7

---
+++ prmn2022a/src/lecture05/Exercise5_7.java
@@ -0,0 +1,18 @@
+package lecture05;
+
+import java.util.ArrayList;
+
+public class Exercise5_7 {
+
+    public static void main(String[] args) {
+        ArrayList<Insect> list = new ArrayList<>();
+        list.add(new Insect());
+        list.add(new Butterfly());
+        list.add(new Locust());
+        list.add(new SwallowtailButterfly());
+
+        for(Insect insect : list){
+            insect.move();
+        }
+    }
+}
